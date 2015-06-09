5b83390315ab768daf4314a089e5330ae0796ae8
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-06-09 10:58:34 +0530

    Paritially completed GameTest

--- test/GameTest.java
+++ test/GameTest.java
@@ -4,11 +4,13 @@ public class GameTest {
 
     @Test
     public void gameShouldInitializeTheSeed() {
-        Game game = new Game(10, 10);
+        Game game = new Game(3, 3);
 
-        boolean[][] initialGrid = new boolean[][]{{false, true, true},
+        boolean[][] initialGrid = new boolean[][]{
+                {false, true, true},
                 {false, false, false},
-                {true, false, true}};
+                {true, false, true}
+        };
         game.setSeed(initialGrid);
     }
 }
